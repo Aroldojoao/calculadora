abc71d3407ff1df06b098752f00c3fee668102ff
aroldojoao <aroldo.joao@hotmail.com>
2021-12-02 22:59:03 -0300

    my first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aroldo_MVC_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            ViewBag.Message = "Sua Aplicação descrita na Pagina";
+
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            ViewBag.Message = "Sua pagina de Contato";
+
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            ViewBag.Message = "Bem vindo, Preencha seus dados";
+
+            return View();
+        }
+        
+        public ActionResult Produtos()
+        {
+            ViewBag.Message = "Produtos";
+
+            return View();
+        }
+    }
+}
